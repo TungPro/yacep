24b228accb2cc7b43c6e3223e677710037437d89
thangtd90 <thangtd90@gmail.com>
2014-09-22 20:55:52 +0700

    Improve icon display. Change data info at Popup page

--- README.md
+++ README.md
@@ -4,21 +4,21 @@ Yet Another Chatwork Emoticons Plus
 "Yet Another Chatwork Emoticons Plus" (YACEP) is a free Chrome Extension which provides more funny emoticons for Chatwork.
 It is made by Framgia's Members, and for Framgia's Members.
 
-YACEP is based on the original CWEP (Chatwork Emoticon Plus) made by `Nguyen Anh Tien` !
+YACEP is based on the original CWEP (Chatwork Emoticon Plus) made by [Nguyen Anh Tien](https://github.com/vigov5) !
 Great thanks to him for his hard work :D
 
 Main Features
 --------------
-* Switch between multiple Emoticon versions
-* Easily update Emoticons list
-* Use custom Emoticons list
-* Add, Edit, and Remove emoticons by your self
-* Sync all your setting via Google Account
+* Switch between multiple Emoticon versions (Under construction)
+* Easily update Emoticons list (Completed)
+* Use custom Emoticons list (Under construction)
+* Add, Edit, and Remove emoticons by your self (Under construction)
+* Sync all your setting via Google Account (Completed)
 * And more ...
 
 External Links
 --------------
 
 * [Homepage](http://yacep.thangtd.com)
-* [Google Chrome Webstore Link (Coming soon ...](#)
+* [Google Chrome Webstore Link (Coming soon ...)](#)
 
--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,5 +1,6 @@
 // Const
 var LOCAL_STORAGE_DATA_KEY = "YACEP_EMO_DATA";
+var LOCAL_STORAGE_INFO_KEY = "YACEP_EMO_INFO";
 
 var CHROME_SYNC_KEY = "YACEP_CHROME_SYNC_DATA";
 
@@ -7,20 +8,20 @@ var DEFAULT_DATA_URL = "https://dl.dropboxusercontent.com/s/ik69cu3wnya6i0d/defa
 
 
 chrome.storage.sync.get(CHROME_SYNC_KEY, function(info) {
-    info = JSON.stringify(info[CHROME_SYNC_KEY]);
-    var url;
+    info = info[CHROME_SYNC_KEY];
+    var url = "";
     if (!$.isEmptyObject(info)) {
         console.log("Getting Data Info from Chrome Storage");
         url = info.data_url;
     }
-    if (!url) {
+    if (url == "") {
         console.log("No information from Chrome Storage. Using default Version");
         url = DEFAULT_DATA_URL;
     }
-    getData(url);
+    getData(url, info);
 });
 
-function getData(url) {
+function getData(url, info) {
     $.getJSON(url)
         .done(function(data) {
             if (typeof(data.data_version) !== 'undefined' && typeof(data.emoticons) !== 'undefined') {
@@ -28,6 +29,13 @@ function getData(url) {
                 console.log("You are using Yacep!" + ". Data Name: " + data.data_name + ". Data Version: "
                 + data.data_version);
                 localStorage[LOCAL_STORAGE_DATA_KEY] = JSON.stringify(data.emoticons);
+                info.data_name = data.data_name;
+                info.data_version = data.data_version;
+                info.date_sync = (new Date).toLocaleString();
+                var sync = {};
+                sync[CHROME_SYNC_KEY] = info;
+                chrome.storage.sync.set(sync, function() {
+                });
                 addInjectedScript();
             }
         }).fail(function( jqxhr, textStatus, error ) {
